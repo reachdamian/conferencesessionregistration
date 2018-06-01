1c1edb6d7f4b89357ca017d81a65c65ef9e55570
Damian Chambers <damian.chambers@yahoo.com>
2018-06-01 16:09:50 -0500

    Update Institution and Methods for Uploading Lists

--- cjcsessionapp/App_Start/BundleConfig.cs
+++ cjcsessionapp/App_Start/BundleConfig.cs
@@ -12,7 +12,8 @@ namespace cjcsessionapp
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                         "~/Scripts/jquery.validate.js",
+                        "~/Scripts/jquery.validate.unobtrusive.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -21,6 +22,7 @@ namespace cjcsessionapp
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
+
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
